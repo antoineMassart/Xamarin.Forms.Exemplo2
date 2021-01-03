8efcd09f2e90c3fcf9688d70b72c193f9ea0682e
User <User@DESKTOP-RQF2T9L>
2021-01-03 02:41:00 +0000

    final version.

--- Exemplo2/Exemplo2/ContentsPage.xaml.cs
+++ Exemplo2/Exemplo2/ContentsPage.xaml.cs
@@ -14,37 +14,37 @@ namespace Exemplo2
     {
 
 
-        public class ClassListaImagemTextoNumero
+        public class ClassListContent
         {
-            public ImageSource Imagem { get; set; }
-            public string Texto { get; set; }
-            public int Numero { get; set; }
+            public ImageSource Image { get; set; }
+            public string Text { get; set; }
+            public int Num { get; set; }
         }
-
         
+
 
         public ContentsPage()
         {
             InitializeComponent();
 
-            var ListaImagemTextoNumero = new List<ClassListaImagemTextoNumero>();
-            
-        
-            ListaImagemTextoNumero.Add(new ClassListaImagemTextoNumero{Imagem = ImageSource.FromResource("Exemplo2.Images.pinnaple.png"),Texto = "Pinnaple", Numero = 1});
-            ListaImagemTextoNumero.Add(new ClassListaImagemTextoNumero { Imagem = ImageSource.FromResource("Exemplo2.Images.apple.png"), Texto = "Apple", Numero = 2 });
-            ListaImagemTextoNumero.Add(new ClassListaImagemTextoNumero { Imagem = ImageSource.FromResource("Exemplo2.Images.cherri.png"), Texto = "Cherry", Numero = 3 });
-            ListaImagemTextoNumero.Add(new ClassListaImagemTextoNumero { Imagem = ImageSource.FromResource("Exemplo2.Images.mango.png"), Texto = "Mango", Numero = 4 });
-            ListaImagemTextoNumero.Add(new ClassListaImagemTextoNumero { Imagem = ImageSource.FromResource("Exemplo2.Images.strawberry.png"), Texto = "Strawbery", Numero = 5 });
+            var ListContent = new List<ClassListContent>();
+
+
+            ListContent.Add(new ClassListContent {Image = ImageSource.FromResource("Exemplo2.Images.pinnaple.png"),Text = "Pinnaple", Num = 1});
+            ListContent.Add(new ClassListContent { Image = ImageSource.FromResource("Exemplo2.Images.apple.png"), Text = "Apple", Num = 2 });
+            ListContent.Add(new ClassListContent { Image = ImageSource.FromResource("Exemplo2.Images.cherri.png"), Text = "Cherry", Num = 3 });
+            ListContent.Add(new ClassListContent { Image = ImageSource.FromResource("Exemplo2.Images.mango.png"), Text = "Mango", Num = 4 });
+            ListContent.Add(new ClassListContent { Image = ImageSource.FromResource("Exemplo2.Images.strawberry.png"), Text = "Strawbery", Num = 5 });
 
 
             for (int i = 0; i < 10; i++)
             {
-                ListaImagemTextoNumero.Add(new ClassListaImagemTextoNumero { Imagem = ImageSource.FromResource("Exemplo2.Images.empty.png"), Texto = "empty", Numero = 0 });
+                ListContent.Add(new ClassListContent { Image = ImageSource.FromResource("Exemplo2.Images.empty.png"), Text = "empty", Num = 0 });
 
             }
 
 
-                listOfContents.ItemsSource = ListaImagemTextoNumero;
+                listOfContents.ItemsSource = ListContent;
         }
     }
 }
